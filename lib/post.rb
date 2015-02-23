c0fcbdfc6a966ea5ebe71923459296c5636e5b3b
axton21 <axton_n@yahoo.com>
2015-02-23 13:17:57 -0500

    post.rb

---
+++ lib/post.rb
@@ -0,0 +1,30 @@
+require 'pry'
+require 'json'
+
+class Post
+attr_reader :title, :url, :upvotes, :downvotes, :content, :image_url
+
+  def initialize(hash)
+    @title = hash[:title]
+    @url = hash[:url]
+    @upvotes = hash[:upvotes]
+    @downvotes = hash[:downvotes]
+    @content = hash[:content]
+    @image_url = hash[:image_url]
+  end
+
+  def to_html
+    # binding.pry
+          "<li>
+            <a href=\"#{url}\">
+                <h1>#{title}</h1>
+                <img src=\"#{image_url}\" />
+                <h4>Upvotes:</p>
+                <p>#{upvotes}</h4>
+                <p>#{content}</p>
+                <p>Downvotes:</p>
+                <h4>#{downvotes}</h4>
+            </a>
+          </li>"
+  end
+end
---
+++ lib/reddit_reader.rb
@@ -0,0 +1,11 @@
+class RedditReader
+
+require 'json'
+require 'rest_client'
+require 'pry'
+
+# reddit_hash = JSON.parse(RestClient.get('http://reddit.com/.json'))
+
+
+
+end
